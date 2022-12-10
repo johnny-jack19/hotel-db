0adbe1462685a158c596d25904e469e2fc42a8c1
Jonathan Jackson <jacksonj9891@gmail.com>
2022-12-10 17:35:02 -0500

    DB connections

--- db/knex.js
+++ db/knex.js
@@ -4,9 +4,9 @@ const connectedKnex = knex({
   client: "mysql",
   connection: {
     host: process.env.CON_HOST,
-    user: "b7a7bf2117500d",
-    password: "4823c815",
-    database: "heroku_26aa1c226845f25",
+    user: process.env.USER_ACCESS,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   },
 });
 
