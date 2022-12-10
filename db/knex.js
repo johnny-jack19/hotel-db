4c6ccbedf45dc5de03e9fcf6b6586adb7b35c344
Jonathan Jackson <jacksonj9891@gmail.com>
2022-12-10 17:27:41 -0500

    test

--- db/knex.js
+++ db/knex.js
@@ -1,13 +1,13 @@
-const knex = require('knex');
+const knex = require("knex");
 
 const connectedKnex = knex({
-    client: 'mysql',
-    connection: {
-        host: 'us-cdbr-east-06.cleardb.net',
-        user: 'b7a7bf2117500d',
-        password: '4823c815',
-        database: 'heroku_26aa1c226845f25'
-    }
+  client: "mysql",
+  connection: {
+    host: process.env.CON_HOST,
+    user: "b7a7bf2117500d",
+    password: "4823c815",
+    database: "heroku_26aa1c226845f25",
+  },
 });
 
 module.exports = connectedKnex;
