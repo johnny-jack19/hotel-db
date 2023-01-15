a83858417d92b9016543fc7997563d46517ed7c2
Jonathan Jackson <jacksonj9891@gmail.com>
2023-01-15 17:52:56 -0500

    Added function to get names

--- db/book.js
+++ db/book.js
@@ -1,86 +1,119 @@
-const knex = require('./knex');
+const knex = require("./knex");
 
 //Only for making calendar days
 function createDay(day) {
-    return knex('rooms').insert(day);
+  return knex("rooms").insert(day);
 }
 
 //Billing
-function  makeCustomer(customer) {
-    return knex('billing').insert(customer);
+function makeCustomer(customer) {
+  return knex("billing").insert(customer);
 }
 
 function deleteBilling(id) {
-    return knex('billing').where('customerId', id).del();
+  return knex("billing").where("customerId", id).del();
 }
 
 //Booking
 function makeBooking(booking) {
-    return knex('booking').insert(booking);
+  return knex("booking").insert(booking);
 }
 
 function deleteBooking(id) {
-    return knex('booking').where('id', id).del();
+  return knex("booking").where("id", id).del();
 }
 
 //Rooms
 function deleteBookingFromRooms(room, booking) {
-    return knex('rooms').where(room, booking).update({
-        [room]: 0
+  return knex("rooms")
+    .where(room, booking)
+    .update({
+      [room]: 0,
     });
 }
 
 function addBookingToRooms(room, booking, startDay, endDay) {
-    return knex('rooms').whereBetween('bookDay', [startDay, endDay]).update({
-        [room]: booking
+  return knex("rooms")
+    .whereBetween("bookDay", [startDay, endDay])
+    .update({
+      [room]: booking,
     });
 }
 
 //Occupied
 function updateOccupied(room, id) {
-    return knex('occupied').select('*').update({[room]: id});
+  return knex("occupied")
+    .select("*")
+    .update({ [room]: id });
 }
 //Gets
 function getCustomer(field, value) {
-    return knex('billing').where(field, value).select('*');
+  return knex("billing").where(field, value).select("*");
 }
 
 function getBooking(field, value) {
-    return knex('booking').where(field, value).select('*');
+  return knex("booking").where(field, value).select("*");
 }
 
 function getCalendar() {
-    return knex('rooms').select('*');
+  return knex("rooms").select("*");
 }
 
 function getDay(day) {
-    return knex('rooms').where('bookDay', day).select('*');
+  return knex("rooms").where("bookDay", day).select("*");
 }
 
 function getDayRange(startDay, endDay) {
-    return knex('rooms').whereBetween('bookDay', [startDay, endDay]).select('*');
+  return knex("rooms").whereBetween("bookDay", [startDay, endDay]).select("*");
 }
 
 function getOccupied() {
-    return knex('occupied').select('*');
+  return knex("occupied").select("*");
 }
 
 function getBookingAndCustomer(id) {
-    return knex('booking').where('id', id).join('billing', 'booking.customerId', '=', 'billing.customerId')
-    .select('*');
+  return knex("booking")
+    .where("id", id)
+    .join("billing", "booking.customerId", "=", "billing.customerId")
+    .select("*");
 }
 
 function getLookUp(field, value) {
-    return knex('booking').where(field, value).join('billing', 'booking.customerId', '=', 'billing.customerId')
-    .select('*');
+  return knex("booking")
+    .where(field, value)
+    .join("billing", "booking.customerId", "=", "billing.customerId")
+    .select("*");
+}
+
+async function getGuestOnDay(day) {
+  let myDay = await getDay(day);
+  let ids = [
+    myDay[0]["room1"],
+    myDay[0]["room2"],
+    myDay[0]["room3"],
+    myDay[0]["room4"],
+    myDay[0]["room5"],
+    myDay[0]["room6"],
+  ];
+  return knex("booking").whereIn("id", ids).select("name");
 }
 
 module.exports = {
-    createDay, makeCustomer, makeBooking,
-    deleteBooking, deleteBilling, getCustomer,
-    addBookingToRooms, deleteBookingFromRooms,
-    getBooking, getCalendar,
-    getDay, getDayRange, getOccupied,
-    getBookingAndCustomer, updateOccupied,
-    getLookUp
+  createDay,
+  makeCustomer,
+  makeBooking,
+  deleteBooking,
+  deleteBilling,
+  getCustomer,
+  addBookingToRooms,
+  deleteBookingFromRooms,
+  getBooking,
+  getCalendar,
+  getDay,
+  getDayRange,
+  getOccupied,
+  getBookingAndCustomer,
+  updateOccupied,
+  getLookUp,
+  getGuestOnDay,
 };
--- index.js
+++ index.js
@@ -1,123 +1,131 @@
-const express = require('express');
-const cors = require('cors');
-const db = require('./db/book');
+const express = require("express");
+const cors = require("cors");
+const db = require("./db/book");
 
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 
-
-
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 //Make calendar
-app.post('/make', async (req, res) => {
-    const results = await db.createDay(req.body);
-    res.status(201).json({id: results[0]});
+app.post("/make", async (req, res) => {
+  const results = await db.createDay(req.body);
+  res.status(201).json({ id: results[0] });
 });
 
 //**********************Billing table*********************************
 //Make customer
-app.post('/billing', async (req, res) => {
-    const results = await db.makeCustomer(req.body);
-    res.status(201).json({results});
+app.post("/billing", async (req, res) => {
+  const results = await db.makeCustomer(req.body);
+  res.status(201).json({ results });
 });
 
-app.delete('/billing/:id', async (req, res) => {
-    await db.deleteBilling(req.params.id);
-    res.status(200).json({success: true});
+app.delete("/billing/:id", async (req, res) => {
+  await db.deleteBilling(req.params.id);
+  res.status(200).json({ success: true });
 });
 
 //***********************Rooms table***********************************
-//Set rooms to null (call after delete booking) 
-app.patch('/rooms/:room/:booking', async (req, res) => {
-    await db.deleteBookingFromRooms(req.params.room, req.params.booking);
-    res.status(201).json({success: true});
+//Set rooms to null (call after delete booking)
+app.patch("/rooms/:room/:booking", async (req, res) => {
+  await db.deleteBookingFromRooms(req.params.room, req.params.booking);
+  res.status(201).json({ success: true });
 });
 
 //Set rooms to booking id (call after make booking)
-app.patch('/rooms/:room/:booking/:startDay/:endDay', async (req, res) => {
-    await db.addBookingToRooms(req.params.room, req.params.booking, req.params.startDay, req.params.endDay);
-    res.status(201).json({success: true});
-})
+app.patch("/rooms/:room/:booking/:startDay/:endDay", async (req, res) => {
+  await db.addBookingToRooms(
+    req.params.room,
+    req.params.booking,
+    req.params.startDay,
+    req.params.endDay
+  );
+  res.status(201).json({ success: true });
+});
 
 //************************Booking table**********************************
 //Make booking
-app.post('/booking', async (req, res) => {
-    const results = await db.makeBooking(req.body);
-    res.status(201).json({results});
+app.post("/booking", async (req, res) => {
+  const results = await db.makeBooking(req.body);
+  res.status(201).json({ results });
 });
 
 //Delete booking
-app.delete('/booking/:id', async (req, res) => {
-    await db.deleteBooking(req.params.id);
-    res.status(200).json({success: true});
+app.delete("/booking/:id", async (req, res) => {
+  await db.deleteBooking(req.params.id);
+  res.status(200).json({ success: true });
 });
 
 //Update booking (not used)
-app.patch('/booking/:id', async (req, res) => {
-    await db.processBooking(req.params.id);
-    res.status(201).json({success: true});
-
+app.patch("/booking/:id", async (req, res) => {
+  await db.processBooking(req.params.id);
+  res.status(201).json({ success: true });
 });
 
 //************************Occupied Table**********************************
 //Update
-app.patch('/occupied/:room/:id', async (req, res) => {
-    await db.updateOccupied(req.params.room, req.params.id);
-    res.status(201).json({success: true});
+app.patch("/occupied/:room/:id", async (req, res) => {
+  await db.updateOccupied(req.params.room, req.params.id);
+  res.status(201).json({ success: true });
 });
 
 //******************************Gets**************************************
 //Get customer
-app.get('/billing/:field/:value', async (req, res) => {
-    const results = await db.getCustomer(req.params.field, req.params.value);
-    res.status(200).json({results});
+app.get("/billing/:field/:value", async (req, res) => {
+  const results = await db.getCustomer(req.params.field, req.params.value);
+  res.status(200).json({ results });
 });
 
 //Get booking
-app.get('/booking/:field/:value', async (req, res) => {
-    const results = await db.getBooking(req.params.field, req.params.value);
-    res.status(200).json({results});
+app.get("/booking/:field/:value", async (req, res) => {
+  const results = await db.getBooking(req.params.field, req.params.value);
+  res.status(200).json({ results });
 });
 
 //Get calendar
-app.get('/rooms', async (req, res) => {
-    const results = await db.getCalendar();
-    res.status(200).json(results);
+app.get("/rooms", async (req, res) => {
+  const results = await db.getCalendar();
+  res.status(200).json(results);
 });
 
 //Get single day
-app.get('/rooms/:day', async (req, res) => {
-    const results = await db.getDay(req.params.day);
-    res.status(200).json({results});
+app.get("/rooms/:day", async (req, res) => {
+  const results = await db.getDay(req.params.day);
+  res.status(200).json({ results });
+});
+
+//Get guest on a certain day
+app.get("/daysname/:day", async (req, res) => {
+  const results = await db.getGuestOnDay(req.params.day);
+  res.status(200).json({ results });
 });
 
 //Get day range
-app.get('/rooms/:startDay/:endDay', async (req, res) => {
-    const results = await db.getDayRange(req.params.startDay, req.params.endDay);
-    res.status(200).json({results});
+app.get("/rooms/:startDay/:endDay", async (req, res) => {
+  const results = await db.getDayRange(req.params.startDay, req.params.endDay);
+  res.status(200).json({ results });
 });
 
 //Get occupied table
-app.get('/occupied', async (req, res) => {
-    const results = await db.getOccupied();
-    res.status(200).json(results);
+app.get("/occupied", async (req, res) => {
+  const results = await db.getOccupied();
+  res.status(200).json(results);
 });
 
 //Get Booking and Customer info
-app.get('/customer-info/:id', async (req, res) => {
-    const results = await db.getBookingAndCustomer(req.params.id);
-    res.status(200).json({results});
+app.get("/customer-info/:id", async (req, res) => {
+  const results = await db.getBookingAndCustomer(req.params.id);
+  res.status(200).json({ results });
 });
 
 //Get Booking and Customer info by field
-app.get('/customer-info/:field/:value', async (req, res) => {
-    const results = await db.getLookUp(req.params.field, req.params.value);
-    res.status(200).json({results});
+app.get("/customer-info/:field/:value", async (req, res) => {
+  const results = await db.getLookUp(req.params.field, req.params.value);
+  res.status(200).json({ results });
 });
 
 app.listen(PORT, () => {
-    console.log("Server is running on Port " + PORT);
+  console.log("Server is running on Port " + PORT);
 });
